639a823e261308346ef844e4721e70d559079e4f
Nabin <nabin_5778@hotmail.com>
2019-01-31 01:11:25 +0545

    last

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ProductTableSeeder::class);
+        //$this->call(ProductTableSeeder::class);
         $this->call(UserTableSeeder::class);
 
     }
