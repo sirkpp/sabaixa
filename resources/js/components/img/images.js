fb2d8a6ef3ba049538e2d9b5fb0324cc8cc7dd7b
Amit Mahato <073bct505.amit@pcampus.edu.np>
2019-02-01 12:02:17 +0545

    images.js added

---
+++ resources/js/components/img/images.js
@@ -0,0 +1,44 @@
+import mp1 from "./main-product01.jpg";
+import mp2 from "./main-product02.jpg";
+import mp3 from "./main-product03.jpg";
+import mp4 from "./main-product04.jpg";
+import bnr1 from "./banner01.jpg";
+import bnr2 from "./banner02.jpg";
+import bnr3 from "./banner03.jpg";
+import bnr4 from "./banner04.jpg";
+import bnr5 from "./banner05.jpg";
+import bnr6 from "./banner06.jpg";
+import bnr7 from "./banner07.jpg";
+import bnr8 from "./banner08.jpg";
+import bnr9 from "./banner09.jpg";
+import bnr10 from "./banner10.jpg";
+import bnr11 from "./banner11.jpg";
+import bnr12 from "./banner12.jpg";
+import bnr13 from "./banner13.jpg";
+import bnr14 from "./banner14.jpg";
+import bnr15 from "./banner15.jpg";
+import tn1 from "./thumb-product01.jpg";
+import tn2 from "./thumb-product02.jpg";
+import tn3 from "./thumb-product03.jpg";
+import tn4 from "./thumb-product04.jpg";
+
+
+export const imageofprod = [mp1, mp2, mp3, mp4];
+export const thumbofprod = [tn1, tn2, tn3, tn4];
+export const bannerofprod = [
+  bnr1,
+  bnr2,
+  bnr3,
+  bnr4,
+  bnr5,
+  bnr6,
+  bnr7,
+  bnr8,
+  bnr9,
+  bnr10,
+  bnr11,
+  bnr12,
+  bnr13,
+  bnr14,
+  bnr15
+];
